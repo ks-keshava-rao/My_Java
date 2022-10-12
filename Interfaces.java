8b6a7889d22b892d1b829d9b86767b885e7d27a2
kskrao219 <keshava.balaji@anywhere.co>
2022-10-12 10:58:51 +0530

    Interfaces

--- Interfaces.java
+++
@@ -1,4 +0,0 @@
-package interfaceExample;
-public class Interfaces {
-    
-}
---
+++ interfaceExample/Interfaces.java
@@ -0,0 +1,26 @@
+package interfaceExample;
+interface test {
+    void method1();
+    void method2();
+}
+class my implements test {
+    @Override
+    public void method1(){
+        System.out.println("method1");
+    }
+    @Override
+    public void method2(){
+        System.out.println("method2");
+    }
+    public void method3(){
+        System.out.println("method3");
+    }
+}
+public class Interfaces {
+    public static void main(String[] args){
+        test t1 = new my();
+        t1.method1();
+        t1.method2();
+        // t1.method3(); error
+    }
+}
