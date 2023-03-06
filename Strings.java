e7732c12c95a6e0de6ffe45fb50c53190de288ed
kskrao219 <keshava.balaji@anywhere.co>
2023-03-07 01:18:20 +0530

    string basics

--- Strings.java
+++ Strings.java
@@ -1,9 +1,12 @@
 import java.util.*;
 public class Strings {
     public static void main(String[] args){
+        // String is a class used as a datatype 
+        // String str1(reference) = "Java program"(String object,literal) 
         Scanner sc = new Scanner(System.in);
         String str =sc.nextLine();        
         System.out.println(str.charAt(3));
         System.out.println(str.length());
     }
 }
+ 
