144e41aaba8c349fd883d91c7a2492fd805a230a
kskrao219 <keshava.balaji@anywhere.co>
2023-03-10 14:08:28 +0530

    problems

---
+++ MaxElement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MaxElement {
+    public static void main(String[] args){
+        try (Scanner sc = new Scanner(System.in)) {
+            int size;
+            size = sc.nextInt();
+            int[] array = new int[size];
+            for(int i=0; i<array.length ; i++){
+                array[i]=sc.nextInt();
+            }
+            int temp=0;
+            for(int i=0; i<array.length ; i++){
+                if(array[i]>temp){
+                   temp = array[i];
+                }
+            }
+            System.out.println(temp);
+        }
+    }
+}
---
+++ SecondMax.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SecondMax {
+    public static void main(String[] args){
+        try (Scanner sc = new Scanner(System.in)) {
+            int size;
+            size = sc.nextInt();
+            int[] array = new int[size];
+            for(int i=0; i<array.length ; i++){
+                array[i]=sc.nextInt();
+            }
+            int max2;
+            int max=max2=array[0];
+            for(int i=0; i<array.length ; i++){
+                if(array[i]>max ){
+                    max2 = max;
+                    max = array[i];
+                }
+                else if(array[i]>max2){
+                    max2 = array[i];
+                }
+
+            }
+            System.out.println(max2);
+        }
+    }
+}
