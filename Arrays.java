d25bf392121f42a44fa9a8d2f26223e23c3b2a8b
kskrao219 <keshava.balaji@anywhere.co>
2023-03-10 12:27:49 +0530

    arrays

--- Arrays.java
+++ Arrays.java
@@ -4,13 +4,21 @@ public class Arrays {
         Scanner sc = new Scanner(System.in);
         int size ;
         size = sc.nextInt();
-        int[] intArray = new int[size];
+        //initializations 
+        int A[] = {1,2,3,4,8,5}; 
+        int intArray[] = new int[size];
+        int c[];
+        c = new int[10];
+        int []d = new int[5];
+        int[] e = new int[5];
         for(int i=0;i<size;i++){
             intArray[i]=sc.nextInt();
         }
         for(int i=0;i<intArray.length;i++){
             System.out.println(intArray[i]);
         }
-
+       for(int x : intArray){ //foreach loop 
+        System.out.println(x);
+       }
     }
 }
