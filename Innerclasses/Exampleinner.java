bee60275f7f1c151b8b483850882265e8982f0a7
kskrao219 <keshava.balaji@anywhere.co>
2023-04-22 12:52:43 +0530

    inner classes

---
+++ Innerclasses/Exampleinner.java
@@ -0,0 +1,26 @@
+package Innerclasses;
+
+import javax.naming.InvalidNameException;
+
+class outerclass{
+    int x=10;
+    innerclass i = new innerclass(); //is accessible by whole outer class 
+    class innerclass{
+        int y=23;
+        public void innerdisplay(){
+            System.out.println("innerdisplay "+y+" "+x);
+        }
+    }
+    public void outerdisplay(){
+        i.innerdisplay();
+        System.out.println(i.y);
+    }
+}
+public class Exampleinner {
+    public static void main(String args[]){
+        outerclass o = new outerclass();
+        o.outerdisplay();
+        outerclass.innerclass oi = new outerclass().new innerclass();
+        oi.innerdisplay();
+    }
+}
