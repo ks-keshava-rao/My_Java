88032ce15afc49918adf356acb09b241565d7317
kskrao219 <keshava.balaji@anywhere.co>
2023-06-30 19:31:59 +0530

    packages

--- mypack1/Demo.java
+++ mypack1/Demo.java
@@ -1,4 +1,5 @@
 package mypack1;
+//to add a class under a package :- javac -d <directory path> program.java
 public class Demo {
     public void display(){
         System.out.println("this is class demo");
