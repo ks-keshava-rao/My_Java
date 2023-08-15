ddee852d4c562ae675066258d25ffd2b41f5f9aa
kskrao219 <keshava.balaji@anywhere.co>
2023-08-16 00:37:59 +0530

    synchronozation ps

---
+++ Xmultithreading/SyncPS.java
@@ -0,0 +1,17 @@
+package Xmultithreading;
+
+class ATM {
+
+    public void checkBalance(String name){
+        System.out.println("balance for " + name + " $1000");
+    }
+    public void withdraw(String name,int amount){
+        System.out.println("withdrawing the amount $"+amount + "for "+ name );
+     }
+}
+class customer1{
+
+}
+public class SyncPS {
+    
+}
